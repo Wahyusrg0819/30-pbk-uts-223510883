1b1feb0346dd5fcd412f288cdbca096b38d87fba
Wahyusrg0819 <fatina0819@gmail.com>
2024-05-10 06:16:32 +0700

    header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <template>
+
+<header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">TodoList-Wahyu Siregar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" @click="selectedMenu = 'Todos'" :class="{ 'active': selectedMenu === 'Todos' }">Todos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" @click="selectedMenu = 'Post'" :class="{ 'active': selectedMenu === 'Post' }">Post</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+
   <main class="app">
     <!-- Header -->
     
